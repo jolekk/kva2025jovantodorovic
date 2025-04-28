31b3c69d700b0deaef8ce6701c35bb63ba4bbc06
jolekk <kakav.brat@gmail.com>
2025-04-28 15:37:32 +0200

    Small fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
-import { AirlineComponent } from './airline/airline.component';
-import { DetailsComponent } from './details/details.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
@@ -16,9 +14,6 @@ export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'search', component: SearchComponent },
-    { path: 'airlines', component: AirlineComponent },
-    // { path: 'details/:id/order', component: OrderComponent },
-    { path: 'details/:id', component: DetailsComponent },
     { path: 'actor', component: ActorlistComponent },
     { path: 'actor/:id', component: ActorComponent },
     { path: 'movie-details/:id', component: MovieDetailsComponent },
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,7 +5,6 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router, RouterLink } from '@angular/router';
-import { FlightService } from '../../services/flight.service';
 import { MatSelectModule } from '@angular/material/select';
 import { NgFor } from '@angular/common';
 import { UserService } from '../../services/user.service';
@@ -44,9 +43,7 @@ export class SignupComponent {
   public selectedGenre: number | null = null;
 
   public constructor(private router: Router) {
-    FlightService.getDestinations().then(
-      (rsp) => (this.destinationList = rsp.data)
-    );
+    
   }
 
   ngOnInit() {
